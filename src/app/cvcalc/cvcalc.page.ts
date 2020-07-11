9264dcf4c7ab8aace19a099e8c5e1ae6898deaee
Deepak Kumar <deepak.k@tavant.com>
2020-07-11 17:08:10 +0530

    test1 code done

--- src/app/cvcalc/cvcalc.page.ts
+++ src/app/cvcalc/cvcalc.page.ts
@@ -30,10 +30,10 @@ export class CvcalcPage implements OnInit {
   ];
 
   onButtonPress(symbol) {
-    console.log(symbol);
-
+    //console.log(symbol);
+    console.log(this.resultArr);
     if (isNumber(symbol)) {
-      console.log('is a number');
+      //console.log('is a number');
       if (this.readyForNewInput)
         this.value = '' + symbol;
       else
@@ -41,11 +41,24 @@ export class CvcalcPage implements OnInit {
       this.readyForNewInput = false;
     }
     else if (symbol === '>') {
-      this.imgVal = this.imgArr[this.imgPos]
+console.log("this.imgVal>>"+ this.imgVal+"this.value>>>"+this.value)
+
+      if(this.imgVal == this.value) {
+        console.log("true")
+        this.resultArr.push(1)
+        } else {
+          console.log("false")
+          this.resultArr.push(0)
+        }
+      
       this.imgPos++
-      this.resultArr.push(this.value)
+      this.imgVal = this.imgArr[this.imgPos]
+      
       this.value = ''
       this.readyForNewInput = true
+      if(this.imgArr.length === (this.imgPos+1)) {
+        console.log(this.imgArr.length + ">>>>>>"+(this.imgPos+1)+">>>>>>"+"EUQAL")
+      }
 
     }
     else if (symbol === 'skip') {
@@ -53,9 +66,13 @@ export class CvcalcPage implements OnInit {
       this.imgPos++
       this.value = ''
       this.readyForNewInput = true
+      if(this.imgArr.length === (this.imgPos+1)) {
+        console.log(this.imgArr.length + ">>>>>>"+(this.imgPos+1)+">>>>>>"+"EUQAL")
+      }
 
     }
      else { // operator
+      console.log("this.imgVal>>"+ this.imgVal+"this.value>>>"+this.value)
       this.readyForNewInput = true;
       this.imgVal = this.value;
       this.lastOperator = symbol;
